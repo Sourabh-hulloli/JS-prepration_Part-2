2829a9f352196b59a9e8ba7c8b56f20285f842b1
sourabh hulloli <sourabh.hulloli@gmail.com>
2023-06-10 23:42:03 +0530

    after() method and before()

--- howJSWorks-DOM/DOM/assets/js/append,prepend,remove.js
+++ howJSWorks-DOM/DOM/assets/js/append,prepend,remove.js
@@ -3,21 +3,59 @@
 // prepend
 // remove
 
+// before
+// after
+
 // creating HTML Element
-const newtodoItem = document.createElement("li");
+// const newtodoItem = document.createElement("li");
 
 //? 1. creating text node using createTextNode() method.
-const newTextNode = document.createTextNode("Learning Javascript");
+// const newTextNode = document.createTextNode("Learning Javascript");
 
 //? 2. creating text node using textContent
-newTextNode.textContent = "Hello Javascript";
+// newTextNode.textContent = "Hello Javascript";
 
 // adding textnode in to the list
-newtodoItem.appendChild(newTextNode);
-console.log(newtodoItem);
+// newtodoItem.appendChild(newTextNode);
+// console.log(newtodoItem);
 
 // adding newely created element in to todo-list class container
-const todoList = document.querySelector(".todo-list");
-todoList.append(newtodoItem);
+// const todoList = document.querySelector(".todo-list");
+// todoList.append(newtodoItem);
 
 // prepend()
+// const todoList = document.querySelector(".todo-list");
+
+// const langs = [
+//   "HTML",
+//   "CSS",
+//   "JS",
+//   "REACTJS",
+//   "MongoDB",
+//   "ExpressJS",
+//   "NodeJS",
+// ];
+
+// let nodes = langs.map((lang) => {
+//   let li = document.createElement("li");
+//   li.textContent = lang;
+//   return li;
+// });
+// todoList.prepend(...nodes);
+
+// remove the HTML Element
+// const li = todoList.querySelectorAll("li");
+// const todo1 = li[li.length - 1];
+// todo1.remove();
+
+// before
+const todoList = document.querySelector(".todo-list");
+const newlistItem = document.createElement("li");
+const newTextNode = document.createTextNode(
+  "Appending text using before() method"
+);
+newlistItem.append(newTextNode);
+todoList.before(newlistItem);
+
+// after
+todoList.after(newlistItem);
