cd9e6c40564a9e0ed3c1f0e2881cb20a3008351d
sourabh hulloli <sourabh.hulloli@gmail.com>
2023-06-20 18:16:06 +0530

    Error Handling in AJAX

--- AJAX/errorHandling.js
+++ AJAX/errorHandling.js
@@ -9,7 +9,22 @@ xhr.open("GET", URL);
 xhr.onload = () => {
   if (xhr.status === 200 && xhr.status <= 300) {
     const data = JSON.parse(xhr.response);
-    console.log(data);
+    // console.log(data);
+    // need 4th object from the API
+    const id = data[3].id;
+    // console.log(id);
+    const xhrId = new XMLHttpRequest();
+    const URLID = `${URL}/${id}`;
+    xhrId.open("GET", URLID);
+    xhrId.onload = () => {
+      if (xhrId.status === 200 && xhrId.status <= 300) {
+        const dataId = JSON.parse(xhrId.response);
+        console.log(dataId);
+      } else {
+        console.log("Something went wrong");
+      }
+    };
+    xhrId.send();
   } else {
     console.log("Something went wrong");
   }
