5d2b34c615040808f933c9fa2ee461b4b01a7731
sourabh hulloli <sourabh.hulloli@gmail.com>
2023-06-20 12:18:50 +0530

    callbackHellToFlatCode

--- Asynchronous JavaScript/callbackHell/callbackHellToFlatCode.js
+++ Asynchronous JavaScript/callbackHell/callbackHellToFlatCode.js
@@ -17,134 +17,25 @@ const heading7 = document.querySelector(".heading-7");
 //      Six          3s           blue
 //      Seven        1s           brown
 
-//? EX - 1 Calback Hell using setTimeout() method.
-// setTimeout(() => {
-//   heading1.textContent = "One";
-//   heading1.style.color = "violet";
-//   setTimeout(() => {
-//     heading2.textContent = "Two";
-//     heading2.style.color = "purple";
-//     setTimeout(() => {
-//       heading3.textContent = "Three";
-//       heading3.style.color = "red";
-//       setTimeout(() => {
-//         heading4.textContent = "Four";
-//         heading4.style.color = "pink";
-//         setTimeout(() => {
-//           heading5.textContent = "Five";
-//           heading5.style.color = "green";
-//           setTimeout(() => {
-//             heading6.textContent = "Six";
-//             heading6.style.color = "blue";
-//             setTimeout(() => {
-//               heading7.textContent = "Seven";
-//               heading7.style.color = "brown";
-//             }, 1000);
-//           }, 3000);
-//         }, 2000);
-//       }, 1000);
-//     }, 2000);
-//   }, 2000);
-// }, 1000);
-
 //? EX - 2 Pyramid of Doom
-function changeText(
-  element,
-  text,
-  color,
-  time,
-  onSuccessCallback,
-  onFailuerCallback
-) {
-  setTimeout(() => {
-    if (element) {
-      element.textContent = text;
-      element.style.color = color;
-      if (onSuccessCallback) {
-        onSuccessCallback();
+function changeText(element, text, color, time) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (element) {
+        element.textContent = text;
+        element.style.color = color;
+        resolve();
+      } else {
+        reject();
       }
-    } else {
-      if (onFailuerCallback) {
-        onFailuerCallback();
-      }
-    }
-  }, time);
+    }, time);
+  });
 }
-changeText(
-  heading1,
-  "One",
-  "violet",
-  1000,
-  () => {
-    changeText(
-      heading2,
-      "Two",
-      "purple",
-      1000,
-      () => {
-        changeText(
-          heading3,
-          "Three",
-          "red",
-          1000,
-          () => {
-            changeText(
-              heading4,
-              "Four",
-              "pink",
-              1000,
-              () => {
-                changeText(
-                  heading5,
-                  "Five",
-                  "green",
-                  1000,
-                  () => {
-                    changeText(
-                      heading6,
-                      "Six",
-                      "blue",
-                      1000,
-                      () => {
-                        changeText(
-                          heading7,
-                          "Seven",
-                          "brown",
-                          1000,
-                          () => {
-                            console.log("hello");
-                          },
-                          () => {
-                            console.log("Heading7 does not exist");
-                          }
-                        );
-                      },
-                      () => {
-                        console.log("Heading6 does not exist");
-                      }
-                    );
-                  },
-                  () => {
-                    console.log("Heading5 does not exist");
-                  }
-                );
-              },
-              () => {
-                console.log("Heading4 does not exist");
-              }
-            );
-          },
-          () => {
-            console.log("Heading3 does not exist");
-          }
-        );
-      },
-      () => {
-        console.log("Heading2 does not exist");
-      }
-    );
-  },
-  () => {
-    console.log("Heading1 does not exist");
-  }
-);
+
+changeText(heading1, "One", "violet", 1000)
+  .then(() => changeText(heading2, "Two", "purple", 1000))
+  .then(() => changeText(heading3, "Three", "red", 1000))
+  .then(() => changeText(heading4, "Four", "pink", 1000))
+  .then(() => changeText(heading5, "Five", "green", 1000))
+  .then(() => changeText(heading6, "Six", "blue", 1000))
+  .then(() => changeText(heading7, "Seven", "brown", 1000));
